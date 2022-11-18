8c5e8717e7780b8b9c3555653dec49c6e8b6b418
Vivek Bhandari <v_bhandari@outlook.com>
2022-11-18 13:00:05 -0500

    fixed test case

--- app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
+++ app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
@@ -21,7 +21,7 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", "180", aProduct.getPrice());
+        assertEquals(180, aProduct.getPrice(), 0);
     }
 
 }
